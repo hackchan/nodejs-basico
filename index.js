99dc6fb5ac53d7f11a33631ed531f451b20e9566
fabio rojas <hackchan@localhost.localdomain>
2018-12-02 21:27:49 -0500

    se crea index.js para test

---
+++ index.js
@@ -0,0 +1,13 @@
+function a(){
+ console.log('estoy imprimiendo la llamada a la funcion A.')
+}
+
+function b(){
+ console.log('estoy imprimiendo la llamada a la funcion C.')
+}
+
+function c(){
+console.log("estoy imprimiendo la llamada a la funcion C.")
+}
+
+a();b();c();
